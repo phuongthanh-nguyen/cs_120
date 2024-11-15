1d679245c0bfb8f29cfd4fc29e854140f523b539
pnguye10 <88943812+pnguye10@users.noreply.github.com>
2024-11-14 19:52:19 -0500

    error

--- fibo.php
+++ fibo.php
@@ -1,21 +1,21 @@
 <?php
 
-//check if n is a valid input, a non-neg number
+// check if n is a valid input, a non-negative number
 if (isset($_GET['n']) && is_numeric($_GET['n']) && $_GET['n'] >= 0) {
-    //'n' is the nth term
-    //reads value from query string
+    // 'n' is the nth term
+    // reads value from query string
     $n = (int)$_GET['n'];
 
     function fibonacci($n) {
-        $sequence = []; //array to store numbers of the fibo sequence
+        $sequence = []; // array to store numbers of the fibo sequence
         $a = 0;
         $b = 1;
 
         for ($i = 0; $i < $n; $i++) {
             $sequence[] = $a;
             $value = $a + $b;
-            $a = $b; //output of this loop
-            $b = $value; //to be added next loop
+            $a = $b; // output of this loop
+            $b = $value; // to be added next loop
         }
 
         return $sequence;
@@ -23,14 +23,13 @@ if (isset($_GET['n']) && is_numeric($_GET['n']) && $_GET['n'] >= 0) {
 
     $fibSequence = fibonacci($n);
 
-    //contains info for printed response
+    // contains info for printed response
     $response = [
         'length' => $n,
         'fibSequence' => $fibSequence
-
     ];
 
-    //informs API to return as JSON
+    // informs API to return as JSON
     header('Content-Type: application/json');
     echo json_encode($response);
 
